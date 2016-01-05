cb3eb141d17a9ea91bd74c844698da7ba9ad19ad
Toni Anne Romero <tonianne.romero@gmail.com>
2016-01-05 21:44:38 +0800

    changed Projects to carousal style, fixed some indentations

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,4 +14,29 @@ $(document).ready(function() {
   window.addEventListener("hashchange", shiftWindow);
   function load() { if (window.location.hash) shiftWindow(); }
 
+  // Carousel style for Projects
+  // http://bootsnipp.com/snippets/featured/tabbed-slider-carousel
+  $('#myCarousel').carousel({
+    interval: 5000
+  });
+
+  var clickEvent = false;
+  $('#myCarousel').on('click', '.nav a', function() {
+      clickEvent = true;
+      $('.nav li').removeClass('active');
+      $(this).parent().addClass('active');
+  }).on('slid.bs.carousel', function(e) {
+    if(!clickEvent) {
+      var count = $('.nav').children().length -1;
+      var current = $('.nav li.active');
+      current.removeClass('active').next().addClass('active');
+      var id = parseInt(current.data('slide-to'));
+      if(count == id) {
+        $('.nav li').first().addClass('active');
+      }
+    }
+    clickEvent = false;
+  });
+
+
 });
